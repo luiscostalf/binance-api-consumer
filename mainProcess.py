250d338aa7f09485b3d0f62cca85db3a1e5a5b4e
Luís Costa <luiscosta.lf@gmail.com>
2018-01-07 23:21:02 +0000

    changes

--- mainProcess.py
+++ mainProcess.py
@@ -1,13 +1,18 @@
 from binance.client import Client
+from binance.exceptions import BinanceAPIException, BinanceWithdrawException
 import time
-import datetime
+from datetime import datetime
 import sys
 import os
+
 
 class mainProcess:
     def __init__(self, quantity, price, symbol, mode, min_ganho):
         self.client = Client("k8l0u4weAW5Z97sCU2dUX5OuGpis53THcLH20VvyW4rMVkY4KYM9gvY1vuzCSNJ7", "W7W3b2RtUY7h00rCOhKaD0K3el19QYrlLcP4ZstcXwwPStmDdhjskpq7lL4hD9q9")
-        self.quantity = float(quantity)
+        if (quantity >= 1):
+            self.quantity = int(quantity)
+        else:
+            self.quantity = float(quantity)
         self.price = price
         self.symbol = str(symbol)
         self.mode = str(mode)
@@ -15,10 +20,13 @@ class mainProcess:
         self.going = 0
         self.priceBefore = 0
         self.min_ganho = min_ganho
-        self.bot()
         self.totalEarning = 0
         
+        
     def bot(self):
+        log = open(self.symbol + 'lifeLOG.txt', 'a')
+        log.write('\nBot started' + ' Time: ' + str(datetime.now()) + 'PID: ' + str(os.getpid())   )
+        log.write('\nAlive: True' + ' Time: ' + str(datetime.now()) + 'PID: ' + str(os.getpid())   )
         try:
             while(True):
                 trades = self.client.get_open_orders(symbol=self.symbol + 'ETH')
@@ -54,8 +62,8 @@ class mainProcess:
                         f.close()
                         time.sleep(10)
                 else:
-                    f = open(self.symbol + 'statsLOG.txt', 'a')
                     log = open(self.symbol + 'lifeLOG.txt', 'a')
+                    f = open(self.symbol + 'statsLOG.txt', 'a')
                     prices = self.client.get_all_tickers()
                     for price in prices:
                         if price['symbol'] == self.symbol + 'ETH':
@@ -64,11 +72,18 @@ class mainProcess:
                             self.priceBefore = float(price['price'])
                             f.write('\nPrice Now: ' + str(self.priceBefore) +  ' Going Now: ' + str(self.going)  )
                     self.amountBought = (self.quantity * float(trades[0]['price']))
-                    log.write('\nAlive: True' + ' Time: ' + str(datetime.datetime.time()) + 'PID: ' + os.getpid()   )
+                    log.write('\nAlive: True' + ' Time: ' + str(datetime.now()) + 'PID: ' + str(os.getpid())  )
                     time.sleep(300)
                     f.close()
+                    log.close()
         except Exception as e:
+                log = open(self.symbol + 'lifeLOG.txt', 'a')
+                log.write('\nAlive: True' + ' Error: ' + str(e) + 'PID: ' + str(os.getpid())   )
+                log.close()
                 print e
+        finally:
+            pass
+
 
 
     def buy(self):
@@ -76,59 +91,75 @@ class mainProcess:
         flag = True
         f = open(self.symbol + 'workfile.txt', 'a')
         while(flag):
-            priceQuantity = int(self.quantity) * self.price
+            priceQuantity = self.quantity * self.price
             earningETH = self.amount_bought - priceQuantity
             earningFee = earningETH - ((self.amount_bought * 0.001) + (priceQuantity * 0.001))
-            if (earningFee >= self.min_ganho):
+            f.write('\nPrice:' + str(self.price) + ', PriceWithoutFee:' + str(earningETH) + ', PriceWithFee:' + str(earningFee) + ', PriceUsedBuy:' + str(self.amount_bought))
+            if float(earningFee) >= float(self.min_ganho):
                 self.totalEarning =- earningFee
-                f.write('\nPrice:' + str(self.price) + ', PriceWithoutFee:' + str(earningETH) +
-                    ', PriceWithFee:' + str(earningFee) + ', PriceUsedBuy:' + str(self.amount_bought))
                 try: 
-                    log.write('\START SELL quantity:'+ str(self.quantity) + ' price: ' + self.price + ' earningWithFee: ' + earningETH + ' earningWithoutFee: '+ earningFee)    
+                    log.write('\START BUY quantity:'+ str(self.quantity) + ' price: ' + str(self.price) + ' earningWithFee: ' + str(earningETH) + ' earningWithoutFee: '+ str(earningFee))
+                    balance = self.client.get_asset_balance(asset='ETH')
+                    while(balance['free'] < priceQuantity):
+                        log.write('\No funds:'+ str(balance['free']) +  'money needed: ' + str(priceQuantity))
+                        time.sleep(30)
+                        balance = self.client.get_asset_balance(asset='ETH')
                     order = self.client.order_limit_buy(
                         symbol=self.symbol + 'ETH',
-                        quantity=int(self.quantity),
+                        quantity=self.quantity,
                         price=str(self.price))
                     f.write('\quantity:' + str(self.quantity) + 'price:' + str(priceQuantity))
                     self.amountBought = self.quantity * self.price
                     self.mode = 'SELL'
-                    log.write('\END SELL'+ str(datetime.datetime.time()) + ' TOTAL EARNING ' + str(self.totalEarning))
+                    log.write('\END SELL'+ str(datetime.now()) + ' TOTAL EARNING ' + str(self.totalEarning))
                     flag = False
-                except Exception:
-                    log.write('\nERROR SELLING '+ sys.exc_info()[0])
+                except BinanceAPIException as e:
+                    print e.status_code
+                    print e.message
+                    log.write('\nERROR BUYING '+ str(e.message) + 'PID: ' + str(os.getpid()))
+                    log.close()
                     flag = False
                     continue
             else:
-                self.price = self.price - 0.0000001
+                self.price = self.price - 0.00001
         f.close()
+        log.close()
 
     def sell(self):
         log = open(self.symbol + 'sellLOGStatus.txt', 'a')
         f = open(self.symbol + 'sellLOG.txt', 'a')
         flag = True
         while(flag):
-            priceQuantity = int(self.quantity) * self.price
+            priceQuantity = self.quantity * self.price
             earningETH = priceQuantity - self.amount_bought
             earningFee = earningETH -((self.amount_bought * 0.001) + (priceQuantity * 0.001))
-            f.write('\nPrice:' + str(self.price) + ', PriceWithoutFee:' + str(earningETH) +
-                    ', PriceWithFee:' + str(earningFee) + ', PriceUsedBuy:' + str(self.amount_bought))
-            if (earningFee >= self.min_ganho):
-                self.totalEarning = + earningFee
+            if float(earningFee) <= float(self.min_ganho):
+                f.write('\nPrice:' + str(self.price) + ', PriceWithoutFee:' + str(earningETH) + ', PriceWithFee:' + str(earningFee) + ', PriceUsedBuy:' + str(self.min_ganho))
+            if float(earningFee) >= float(self.min_ganho):
+                flag = False
+                self.totalEarning = + earningFee 
                 try: 
-                    log.write('\START SELL quantity:'+ str(self.quantity) + ' price: ' + self.price + ' earningWithFee: ' + earningETH + ' earningWithoutFee: '+ earningFee)
+                    log.write('\START SELL quantity:'+ str(self.quantity) + ' price: ' + str(self.price) + ' earningWithFee: ' + str(earningETH) + ' earningWithoutFee: '+ str(earningFee))
+                    balance = self.client.get_asset_balance(asset=self.symbol)
+                    while(balance['free'] < self.quantity):
+                        log.write('\nNo funds:'+ str(balance['free']) +  'money needed: ' + str(self.quantity))
+                        time.sleep(30)
+                        balance = self.client.get_asset_balance(asset=self.symbol)
                     order = self.client.order_limit_sell(
                         symbol=self.symbol + 'ETH',
-                        quantity=int(self.quantity),
+                        quantity=self.quantity,
                         price=str(self.price))
                     f.write('\quantity:' + str(self.quantity) + 'price:' + str(priceQuantity))
                     self.amountBought = self.quantity * self.price
                     self.mode = 'BUY'
-                    log.write('\END SELL'+ str(datetime.datetime.time()) + ' TOTAL EARNING ' + str(self.totalEarning))
+                    log.write('\END SELL'+ str(datetime.now()) + ' TOTAL EARNING ' + str(self.totalEarning))
                     flag = False
-                except Exception:
-                    log.write('\nERROR SELLING '+ sys.exc_info()[0])
+                except BinanceAPIException as e:
+                    print e.status_code
+                    print e.message
+                    log.write('\nERROR SELLING '+ str(e.message) + 'PID: ' + str(os.getpid()))
                     flag = False
-                    continue
             else:
-                self.price = +self.price + 0.0000001
+                self.price = +self.price + 0.000001
         f.close()
+        log.close()
--- service.py
+++ service.py
@@ -1,8 +1,12 @@
+
 from flask import Flask
 from flask import request
 import mainProcess
 import time
 import subprocess
+import threading
+import json
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,7 +16,7 @@ def hello():
 def start():
     quantity = request.args.get('x', default = 0, type = int)
     print quantity
-    price = request.args.get('y', default = '*', type = str)
+    price = float(request.args.get('y', default = '*', type = str))
     print price
     symbol = request.args.get('z', default = '*', type = str)
     print symbol
@@ -20,9 +24,23 @@ def start():
     print mode
     min_ganho = request.args.get('up', default = '*', type = str)
     print min_ganho
-    mainProcess.mainProcess(quantity,price,symbol,mode,min_ganho)
+    aux = mainProcess.mainProcess(quantity,price,symbol,mode,min_ganho)
+    t = threading.Thread(target=aux.bot)
+    t.start()
     return "Hello World!"
     
+@app.route("/check")
+def check():
+    log = request.args.get('x', default = '*', type = str)
+    f = (str(log) + 'lifeLOG.txt', 'r')
+    obj = json.load(f.read())
+    return obj
+
+@app.route("/kill")
+def kill():
+    log = request.args.get('pid', default = '*', type = str)
+    os.kill(log)
+    return json
 
 
 
